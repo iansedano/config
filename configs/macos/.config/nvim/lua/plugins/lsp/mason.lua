5d73d26deff5155db4d1b615785c35fe092ca461
Ian Currie <ian.currie@bynder.com>
2025-07-03 09:31:40 +0200

    fix nvim lsp setup

--- configs/macos/.config/nvim/lua/plugins/lsp/mason.lua
+++ configs/macos/.config/nvim/lua/plugins/lsp/mason.lua
@@ -12,6 +12,9 @@ return {
     -- import mason-lspconfig
     local mason_lspconfig = require("mason-lspconfig")
 
+    -- import lspconfig
+    local lspconfig = require("lspconfig")
+
     local mason_tool_installer = require("mason-tool-installer")
 
     -- enable mason and configure icons
@@ -32,7 +35,87 @@ return {
         "lua_ls",
         "pyright",
       },
-      automatic_installation = true
+      automatic_installation = true,
+      handlers = {
+        -- default handler for installed servers
+        function(server_name)
+          lspconfig[server_name].setup({
+            capabilities = require("cmp_nvim_lsp").default_capabilities(),
+          })
+        end,
+        ["svelte"] = function()
+          -- configure svelte server
+          lspconfig["svelte"].setup({
+            capabilities = require("cmp_nvim_lsp").default_capabilities(),
+            on_attach = function(client, bufnr)
+              vim.api.nvim_create_autocmd("BufWritePost", {
+                pattern = { "*.js", "*.ts" },
+                callback = function(ctx)
+                  -- Here use ctx.match instead of ctx.file
+                  client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
+                end,
+              })
+            end,
+          })
+        end,
+        ["graphql"] = function()
+          -- configure graphql language server
+          lspconfig["graphql"].setup({
+            capabilities = require("cmp_nvim_lsp").default_capabilities(),
+            filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
+          })
+        end,
+        ["emmet_ls"] = function()
+          -- configure emmet language server
+          lspconfig["emmet_ls"].setup({
+            capabilities = require("cmp_nvim_lsp").default_capabilities(),
+            filetypes = {
+              "html",
+              "typescriptreact",
+              "javascriptreact",
+              "css",
+              "sass",
+              "scss",
+              "less",
+              "svelte",
+            },
+          })
+        end,
+        ["lua_ls"] = function()
+          -- configure lua server (with special settings)
+          lspconfig["lua_ls"].setup({
+            capabilities = require("cmp_nvim_lsp").default_capabilities(),
+            settings = {
+              Lua = {
+                -- make the language server recognize "vim" global
+                diagnostics = {
+                  globals = { "vim" },
+                },
+                completion = {
+                  callSnippet = "Replace",
+                },
+              },
+            },
+          })
+        end,
+        ["pyright"] = function()
+          -- configure pyright server (with special settings)
+          lspconfig["pyright"].setup({
+            capabilities = require("cmp_nvim_lsp").default_capabilities(),
+            settings = {
+              pyright = {
+                disableOrganizeImports = true,
+              },
+              python = {
+                analysis = {
+                  useLibraryCodeForTypes = true,
+                  typeCheckingMode = "basic",
+                },
+              },
+            },
+          })
+        end,
+      }
     })
 
     -- use mason_tool_installer to install other non lsp tools
