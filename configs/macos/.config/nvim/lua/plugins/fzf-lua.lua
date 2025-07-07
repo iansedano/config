98ca67dce7d166795c1e5f3f0fa2bbda72c9cd0c
Ian Currie <ian.currie@bynder.com>
2025-07-07 10:05:24 +0200

    fzf lua include hidden

--- configs/macos/.config/nvim/lua/plugins/fzf-lua.lua
+++ configs/macos/.config/nvim/lua/plugins/fzf-lua.lua
@@ -1,15 +1,15 @@
 return {
 	"ibhagwan/fzf-lua",
 	keys = {
-		{ "<leader>p", ":FzfLua files<CR>", "n", desc = "Find File" },
-		{ "<leader>P", ":FzfLua commands<CR>", "n", desc = "Command Palette" },
-		{ "<leader>fb", ":FzfLua buffers<CR>", "n", desc = "Find Buffer" },
-		{ "<leader>fz", ":FzfLua<CR>", "n", desc = "All FzfLua menus" },
-		{ "<leader>fg", ":FzfLua live_grep<CR>", "n", desc = "Rip Grep" },
+		{ "<leader>p", "<cmd>FzfLua files<cr>", "n", desc = "Find File" },
+		{ "<leader>P", "<cmd>FzfLua commands<cr>", "n", desc = "Command Palette" },
+		{ "<leader>fb", "<cmd>FzfLua buffers<cr>", "n", desc = "Find Buffer" },
+		{ "<leader>fz", "<cmd>FzfLua<cr>", "n", desc = "All FzfLua menus" },
+		{ "<leader>fg", "<cmd>FzfLua live_grep<cr>", "n", desc = "Rip Grep" },
 	},
 	opts = {
 		grep = {
-			rg_opts = "--column --line-number --no-heading --color=always --hidden --smart-case --max-columns=4096 -e",
+			hidden = true,
 		},
 	},
 }
